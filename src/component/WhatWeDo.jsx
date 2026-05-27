32347941c889763f671e5e0af8313519f5510f70
oyeniyiisaac <isaacoyeniyi6@gmail.com>
2026-05-27 14:33:41 +0100

    feat: add growth platforms section with responsive cards and AOS animations

--- src/component/WhatWeDo.jsx
+++ src/component/WhatWeDo.jsx
@@ -27,6 +27,28 @@ const WhatWeDo = () => {
                         <p>People & Culture</p>
                     </div>
                 </div>
+                <div className='growth-platforms-container' data-aos="fade-right" data-aos-duration="1000">
+                    <h1>Our growth platforms</h1>
+                    <div className='growth-platforms'>
+                        <div className='growth-card-container'>
+                            <div className='growth-card1'>
+                                <img src="https://www.mtn.com/wp-content/uploads/2024/07/Logo.svg" alt="" />
+                                <p>We want to be African's leading digital platform</p>
+                                <img src="https://www.mtn.com/wp-content/themes/mtn-refresh/public/img/svgs/Union.svg" className='growth-arrow' alt="" />
+                            </div>
+                            <div className='growth-card2'>
+                                <img src="https://www.mtn.com/wp-content/uploads/2024/07/Logo-4.svg" alt="" />
+                                <p>Businesses operating in the modern context should always strive to improve productivity.</p>
+                                <img src="https://www.mtn.com/wp-content/themes/mtn-refresh/public/img/svgs/Union.svg" className='growth-arrow' alt="" />
+                            </div>
+                            <div className='growth-card3'>
+                                <img src="https://www.mtn.com/wp-content/uploads/2024/07/Logo-1.svg" alt="" />
+                                <p>Our next-generation services company connecting Africa with digital solutions.</p>
+                                <img src="https://www.mtn.com/wp-content/themes/mtn-refresh/public/img/svgs/Union.svg" className='growth-arrow' alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
